1c283d8d6c38fc391afc213b3d131ef52e44eb2b
José Chaparro <jc_chaparro@hotmail.com>
2025-04-11 19:36:02 -0600

    implemented list jobs by product endpoint

--- app/routers/list_jobs.py
+++ app/routers/list_jobs.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, status
 from sqlmodel import select
 from db import SessionDep
-from models import Job
+from models import Job, Product
 from typing import List
 from sqlmodel import SQLModel
 
@@ -56,5 +56,56 @@ def list_jobs(session: SessionDep):
 
     # Transformar a la respuesta deseada
     job_list = [JobList(job_code=job.job_code) for job in jobs]
+
+    return job_list
+
+@router.get("/list-by-product/{product_name}", response_model=List[JobList],
+                summary="List all jobs",
+                response_description="Returns a list of all jobs",
+                tags=["Jobs"],  # Agrupa en la sección "Jobs"
+                status_code=status.HTTP_200_OK,
+            )
+def list_jobs(product_name: str, session: SessionDep):
+    """
+    ## Endpoint to list all jobs
+
+    This endpoint retrieves a list of all jobs available in the system.
+
+    ### Returns:
+    - **List[JobList]**: A list of all jobs, each containing the job code.
+
+    ### Example Usage:
+    ```http
+    GET /jobs/list
+
+    Response:
+    [
+        {
+            "job_code": "JOB123"
+        },
+        {
+            "job_code": "JOB456"
+        },
+        ...
+    ]
+    ```
+
+    ### Workflow:
+    1. Query the database to retrieve all jobs.
+    2. Transform the job data into the desired response format.
+    3. Return the list of jobs.
+    """
+
+    query = (
+        select(Job)
+        .join(Product, Job.product_id == Product.product_id)
+        .where(Product.product_name == product_name)
+    )
+
+    # Obtener todos los Jobs disponibles
+    results = session.exec(query).all()
+
+    # Transformar a la respuesta deseada
+    job_list = [JobList(job_code=job.job_code) for job in results]
 
     return job_list
